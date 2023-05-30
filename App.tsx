4ac6761df795ab20c22c5b47d27c1d58a1a146e4
Mohamed Allam <m.allam@sewedy.com.eg>
2023-05-30 15:28:29 +0000

    refactor rsa

--- App.tsx
+++ App.tsx
@@ -28,10 +28,10 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-  const [pubKey, setPublicKey] = useState('');
-  const [privateKey, setPrivateKey] = useState('');
-  const [encryptedMessage, setEncryptedMessage] = useState('');
-  const [decryptedMessage, setDecryptedMessage] = useState('');
+  const [rsaPublicKey, setRSAPublicKey] = useState('');
+  const [rsaPrivateKey, setRSAPrivateKey] = useState('');
+  const [rsaEncryptedMessage, setRSAEncryptedMessage] = useState('');
+  const [rsaDecryptedMessage, setRSADecryptedMessage] = useState('');
 
   return (
     <SafeAreaView>
@@ -41,19 +41,24 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <View style={{margin: 5}}>
+        <View style={{
+            backgroundColor: '#03fc6f',
+          }}>
+        <View style={{margin: 5}}>
             <Button
               title="Generate Keys"
               onPress={() => {
                 RSA.generateKeys(2048) // set key size
                   .then(keys => {
-                    setPublicKey(keys.public);
-                    setPrivateKey(keys.private);
-                    setDecryptedMessage('');
-                    setEncryptedMessage('');
+                    setRSAPublicKey(keys.public);
+                    setRSAPrivateKey(keys.private);
+                    setRSADecryptedMessage('');
+                    setRSAEncryptedMessage('');
                   });
               }}
             />
+            <Text style={styles.text}>{rsaPublicKey}</Text>
+            <Text>{rsaPrivateKey}</Text>
           </View>
 
           <View style={{margin: 5}}>
@@ -61,40 +66,29 @@ function App(): JSX.Element {
               title="Encrypt"
               onPress={() => {
                 let message = 'my secret message';
-                RSA.encrypt(message, pubKey).then(encodedMessage => {
-                  setEncryptedMessage(encodedMessage);
-                  //console.log(`the encoded message is ${encryptedMessage}`);
+                RSA.encrypt(message, rsaPublicKey).then(encodedMessage => {
+                  setRSAEncryptedMessage(encodedMessage);
                 });
               }}
             />
+            <Text>{rsaEncryptedMessage}</Text>
           </View>
           <View style={{margin: 5}}>
             <Button
               title="Decrypt"
               onPress={() => {
-                RSA.decrypt(encryptedMessage, privateKey).then(
+                RSA.decrypt(rsaEncryptedMessage, rsaPrivateKey).then(
                   decryptedMessage => {
-                    setDecryptedMessage(decryptedMessage);
+                    setRSADecryptedMessage(decryptedMessage);
                     // console.log(`The original message was ${origianlMessage}`);
                   },
                 );
               }}
             />
+            <Text style={styles.text}>{rsaDecryptedMessage}</Text>
           </View>
-          <View style={{margin: 5}}>
-            <Text style={styles.text}>
-              {'decryptedMessage ' + decryptedMessage}
-            </Text>
-          </View>
-          <View style={{margin: 5}}>
-            <Text>{'encryptedMessage ' + encryptedMessage}</Text>
-          </View>
-          <View style={{margin: 5}}>
-            <Text style={styles.text}>{'pubKey ' + pubKey}</Text>
-          </View>
-          <View style={{margin: 5}}>
-            <Text>{'privateKey ' + privateKey}</Text>
-          </View>
+        </View>
+   
         </View>
       </ScrollView>
     </SafeAreaView>
