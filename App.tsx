34a3e5846e6526075ddcb2963684fd42262fab07
Mohamed Allam <m.allam@sewedy.com.eg>
2023-05-30 11:11:48 +0000

    create views

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -15,81 +14,82 @@ import {
   Text,
   useColorScheme,
   View,
+  Button
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+        contentInsetAdjustmentBehavior="automatic">
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+       <View style={{margin: 5}}>
+            <Button
+              title="Generate Keys"
+              onPress={() => {
+                // RSA.generateKeys(2048) // set key size
+                //   .then(keys => {
+                //     setPublicKey(keys.public);
+                //     setPrivateKey(keys.private);
+                //     setDecryptedMessage('')
+                //     setEncryptedMessage('');
+                //   });
+              }}
+            />
+          </View>
+
+          <View style={{margin: 5}}>
+            <Button
+              title="Encrypt"
+              onPress={() => {
+                // let message = 'my secret message';
+                // RSA.encrypt(message, pubKey).then(encodedMessage => {
+                //   setEncryptedMessage(encodedMessage);
+                //  //console.log(`the encoded message is ${encryptedMessage}`);
+                // });
+              }}
+            />
+          </View>
+          <View style={{margin: 5}}>
+            <Button
+              title="Decrypt"
+              onPress={() => {
+                // RSA.decrypt(encryptedMessage, privateKey).then(
+                //   decryptedMessage => {
+                //    setDecryptedMessage(decryptedMessage)
+                //    // console.log(`The original message was ${origianlMessage}`);
+                //   },
+                // );
+              }}
+            />
+          </View>
+          {/* <View style={{margin: 5}}>
+            <Text style={styles.text}>{decryptedMessage}</Text>
+          </View>
+          <View style={{margin: 5}}>
+            <Text>{encryptedMessage}</Text>
+          </View>
+          <View style={{margin: 5}}>
+            <Text style={styles.text}>{pubKey}</Text>
+          </View>
+          <View style={{margin: 5}}>
+            <Text>{privateKey}</Text>
+          </View>
+        </View> */}
         </View>
       </ScrollView>
     </SafeAreaView>
