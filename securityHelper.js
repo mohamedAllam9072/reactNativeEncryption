dbda44173b324851356ff8dd7e7eebceff3af779
Mohamed Allam <m.allam@sewedy.com.eg>
2023-05-31 16:57:46 +0000

    enhance

--- securityHelper.js
+++ securityHelper.js
@@ -1,7 +1,6 @@
 import Aes from 'react-native-aes-crypto';
 
-export const aes_generateKey = (password, salt, cost, length) =>
-  Aes.pbkdf2(password, salt, cost, length);
+export const aes_generateKey = salt => Aes.pbkdf2('Default', salt, 5000, 256);
 
 export const aes_encryptData = (text, key, iv) => {
   return Aes.encrypt(text, key, iv, 'aes-256-cbc').then(cipher => ({
