eec4d2c5bdbf0bf075638edc2ef498a6619c9f6d
Mohamed Allam <m.allam@sewedy.com.eg>
2023-05-31 00:42:50 +0000

    Squashed commit of the following:
    commit 7603ecf536fcbd48325ed8296585efa13dd3ab0f
    Author: Mohamed Allam <m.allam@sewedy.com.eg>
    Date:   Wed May 31 00:41:35 2023 +0000
    
        all done alhamdllah
    
    commit 98c359d5851715209bda44b9923873b401b2f01a
    Author: Mohamed Allam <m.allam@sewedy.com.eg>
    Date:   Wed May 31 00:35:55 2023 +0000
    
        finish aes
    
    commit 215aef76b28564dbfaf9c4ee0f5b2f530ea47d2c
    Author: Mohamed Allam <m.allam@sewedy.com.eg>
    Date:   Tue May 30 16:20:12 2023 +0000
    
        aes try3
    
    commit 4ac6761df795ab20c22c5b47d27c1d58a1a146e4
    Author: Mohamed Allam <m.allam@sewedy.com.eg>
    Date:   Tue May 30 15:28:29 2023 +0000
    
        refactor rsa
    
    commit 2bdbcac369ba085dec5eb76d2701e52bdf31f955
    Author: Mohamed Allam <m.allam@sewedy.com.eg>
    Date:   Tue May 30 15:06:39 2023 +0000
    
        clear aes try1 code

--- App.tsx
+++ App.tsx
@@ -5,14 +5,25 @@
  * RSA ===>>>>==>>>  https://github.com/amitaymolko/react-native-rsa-native
  * AES try1 ====>>>===>>> https://github.com/tectiv3/react-native-aes
  * AES try2 ===>>>====>>> https://morioh.com/p/f0328bbf0ea4
+ * AES try3 ====>>>===>>> https://www.npmjs.com/package/crypto-js
  *
  * @format
  */
 
 import React, {useState} from 'react';
 import {RSA} from 'react-native-rsa-native';
-import {NativeModules, Platform} from 'react-native';
 import Aes from 'react-native-aes-crypto';
+
+const generateKey = (password: any, salt: any, cost: any, length: any) =>
+  Aes.pbkdf2(password, salt, cost, length);
+const encryptData = (text: any, key: any, iv: any) => {
+  return Aes.encrypt(text, key, iv, 'aes-256-cbc').then(cipher => ({
+    cipher,
+  }));
+};
+const decryptData = (encryptedData: any, key: any, iv: any) =>
+  Aes.decrypt(encryptedData, key, iv, 'aes-256-cbc');
+
 import {
   SafeAreaView,
   ScrollView,
@@ -28,49 +39,14 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-  const [pubKey, setPublicKey] = useState('');
-  const [privateKey, setPrivateKey] = useState('');
-  const [encryptedMessage, setEncryptedMessage] = useState('');
-  const [decryptedMessage, setDecryptedMessage] = useState('');
-
-  const generateKey =  (password: string, salt: string, cost: number, length: number) =>
-    Aes.pbkdf2(password, salt, cost, length);
-  const encryptData = (text: string, key: any) => {
-    return Aes.randomKey(16).then(iv => {
-      return Aes.encrypt(text, key, iv, 'aes-256-cbc').then(cipher => ({
-        cipher,
-        iv,
-      }));
-    });
-  };
-  const decryptData = (encryptedData: any, key: any) =>
-    Aes.decrypt(encryptedData.cipher, key, encryptedData.iv, 'aes-256-cbc');
-  try {
-    generateKey('Arnold', 'salt', 5000, 256).then(key => {
-      console.log('Key:', key);
-      encryptData('These violent delights have violent ends', key)
-        .then(({cipher, iv}) => {
-          console.log('Encrypted:', cipher);
-
-          decryptData({cipher, iv}, key)
-            .then(text => {
-              console.log('Decrypted:', text);
-            })
-            .catch(error => {
-              console.log(error);
-            });
-
-          Aes.hmac256(cipher, key).then(hash => {
-            console.log('HMAC', hash);
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    });
-  } catch (e) {
-    console.error(e);
-  }
+  const [rsaPublicKey, setRSAPublicKey] = useState('');
+  const [rsaPrivateKey, setRSAPrivateKey] = useState('');
+  const [rsaEncryptedMessage, setRSAEncryptedMessage] = useState('');
+  const [rsaDecryptedMessage, setRSADecryptedMessage] = useState('');
+  const [aesKey, setAesKey] = useState('');
+  const [aes_iv, setAes_iv] = useState('');
+  const [aesEncrypted, setAesEncrypted] = useState('');
+  const [aesDecrypted, setAesDecrypted] = useState('');
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -79,59 +55,118 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <View style={{margin: 5}}>
-            <Button
-              title="Generate Keys"
-              onPress={() => {
-                RSA.generateKeys(2048) // set key size
-                  .then(keys => {
-                    setPublicKey(keys.public);
-                    setPrivateKey(keys.private);
-                    setDecryptedMessage('');
-                    setEncryptedMessage('');
-                  });
-              }}
-            />
+          <View
+            style={{
+              backgroundColor: '#fcba03',
+            }}>
+            <View style={{margin: 5}}>
+              <Button
+                title="AES Generate Key"
+                onPress={async () => {
+                  try {
+                    const IV = await Aes.randomKey(16);
+                    generateKey('Arnold', 'salt', 5000, 256).then(key => {
+                      setAesKey(key);
+                    });
+                    setAes_iv(IV);
+                  } catch (e) {
+                    console.error(e);
+                  }
+                }}
+              />
+              <Text style={styles.text}>{aesKey}</Text>
+              <Text style={styles.text}>{}</Text>
+              <Text style={styles.text}>{aes_iv}</Text>
+              <Button
+                title="AES Encrypt"
+                onPress={() => {
+                  try {
+                    encryptData('my massage', aesKey, aes_iv)
+                      .then(({cipher}) => {
+                        setAesEncrypted(cipher);
+                      })
+                      .catch(error => {
+                        console.log(error);
+                      });
+                  } catch (e) {
+                    console.error(e);
+                  }
+                }}
+              />
+              <Text style={styles.text}>{aesEncrypted}</Text>
+              <Button
+                title="AES Decrypt"
+                onPress={() => {
+                  decryptData(aesEncrypted, aesKey, aes_iv)
+                    .then(text => {
+                      console.log('Decrypted:', text);
+                      setAesDecrypted(text);
+                    })
+                    .catch(error => {
+                      console.log(error);
+                    });
+                }}
+              />
+              <Text style={styles.text}>{aesDecrypted}</Text>
+            </View>
           </View>
+          <View
+            style={{
+              backgroundColor: '#03fc6f',
+            }}>
+            <View style={{margin: 5}}>
+              <Button
+                title="Generate Keys"
+                onPress={() => {
+                  RSA.generateKeys(2048) // set key size
+                    .then(keys => {
+                      setRSAPublicKey(keys.public);
+                      setRSAPrivateKey(keys.private);
+                      setRSADecryptedMessage('');
+                      setRSAEncryptedMessage('');
+                      console.log('rsaPublicKey' + keys.public);
+                    });
+                }}
+              />
+              <Text style={styles.text}>{rsaPublicKey}</Text>
+              <Text>{rsaPrivateKey}</Text>
+            </View>
 
-          <View style={{margin: 5}}>
-            <Button
-              title="Encrypt"
-              onPress={() => {
-                let message = 'my secret message';
-                RSA.encrypt(message, pubKey).then(encodedMessage => {
-                  setEncryptedMessage(encodedMessage);
-                  //console.log(`the encoded message is ${encryptedMessage}`);
-                });
-              }}
-            />
-          </View>
-          <View style={{margin: 5}}>
-            <Button
-              title="Decrypt"
-              onPress={() => {
-                RSA.decrypt(encryptedMessage, privateKey).then(
-                  decryptedMessage => {
-                    setDecryptedMessage(decryptedMessage);
-                    // console.log(`The original message was ${origianlMessage}`);
-                  },
-                );
-              }}
-            />
-          </View>
-          <View style={{margin: 5}}>
-            <Text style={styles.text}>
-              {'decryptedMessage ' + decryptedMessage}
-            </Text>
-          </View>
-          <View style={{margin: 5}}>
-            <Text>{'encryptedMessage ' + encryptedMessage}</Text>
-          </View>
-          <View style={{margin: 5}}>
-            <Text style={styles.text}>{'pubKey ' + pubKey}</Text>
-          </View>
-          <View style={{margin: 5}}>
-            <Text>{'privateKey ' + privateKey}</Text>
+            <View style={{margin: 5}}>
+              <Button
+                title="Encrypt"
+                onPress={() => {
+                  let message = 'my secret message';
+                  const publicKey = `-----BEGIN RSA PUBLIC KEY-----
+                MIIBCgKCAQEA9sAyyYRdXwZHHyKO689LxlWGrvdRkmcFkkQrony6CqVnVylxJr3X
+                TBDuOLq5EpNr13ad/fMZpQjuIWJehPDYOqxSWOJQi9JygpyVF5Nv29F2BZu2eGih
+                n863CAbpkWTz6ntNqzoTX7nwYDYQzjzdGl5/qF4ikxGLVfmZoAWn4mKD/heb6POT
+                OXaOK/QBh455WdSZlZkMtDZJ1HEeV2dW8srw0h5F2g8GD5ZBLTH8wkWZZjKfU+Qt
+                CArseBgAP6nSclIMM26iTpHQtHRvF3XTplTGC8VSEXx6QQebvmmDJbsl0AregN/Z
+                /BN0QsrPH7J4ydsMbTdUUf/9wqoM4Lnx1QIDAQAB
+                -----END RSA PUBLIC KEY-----`;
+                  RSA.encrypt(message, rsaPublicKey).then(encodedMessage => {
+                    console.log('RSA Encrypt}}}}}}} ', encodedMessage);
+                    setRSAEncryptedMessage(encodedMessage);
+                  });
+                }}
+              />
+              <Text>{rsaEncryptedMessage}</Text>
+            </View>
+            <View style={{margin: 5}}>
+              <Button
+                title="Decrypt"
+                onPress={() => {
+                  RSA.decrypt(rsaEncryptedMessage, rsaPrivateKey).then(
+                    decryptedMessage => {
+                      setRSADecryptedMessage(decryptedMessage);
+                      // console.log(`The original message was ${origianlMessage}`);
+                    },
+                  );
+                }}
+              />
+              <Text style={styles.text}>{rsaDecryptedMessage}</Text>
+            </View>
           </View>
         </View>
       </ScrollView>
