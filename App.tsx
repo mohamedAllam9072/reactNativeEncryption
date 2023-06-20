155ba998236cc7659141933ec8062dd1ffc8c778
Mohamed Allam <m.allam@sewedy.com.eg>
2023-06-20 14:18:54 +0300

    add prince rsa public and private and encrypted session key and public key work success and private not work

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -147,8 +148,20 @@ function App(): JSX.Element {
                 CArseBgAP6nSclIMM26iTpHQtHRvF3XTplTGC8VSEXx6QQebvmmDJbsl0AregN/Z
                 /BN0QsrPH7J4ydsMbTdUUf/9wqoM4Lnx1QIDAQAB
                 -----END RSA PUBLIC KEY-----`;
-                  RSA.encrypt(message, rsaPublicKey).then(encodedMessage => {
-                    console.log('RSA Encrypt}}}}}}} ', encodedMessage);
+
+                  const publicKey2 = `-----BEGIN PUBLIC KEY-----
+                MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy7no4fYr7EIf3HU4jBAw
+                S6S3KaRU0SP3PupT2Lo0QbTIjTVzl8N2+UkwmTOckW9aI2zerKZ40RjUFPehxPJe
+                i5v1GL9cERKY06xtV53VvCAKsPJKr4t6ql1WEB5hZY+it/lG6Y2W16GGbygeONx2
+                NH5FU91W1p1Fc7a9Ot3kKlCxEAcGvynOykEgAmkxkpPfV58G2wahOG1L+yC9ScjE
+                7JEbtQh6RDc5pxmMc9V+TERQ9DL3PKQVONA+wpzqTKp2VqfgtkoiRQY2c42nLmpn
+                f4z9RYkTUvmZvEHmZPjRlSt/xTIfnzeKCkH0CKMY74MQVzgrR+nC8e5u4XaIcRPR
+                QQIDAQAB
+                -----END PUBLIC KEY-----`;
+                  RSA.encrypt(message, publicKey2).then(function (
+                    encodedMessage,
+                  ) {
+                    console.log('RSA Encrypt', encodedMessage);
                     setRSAEncryptedMessage(encodedMessage);
                   });
                 }}
@@ -159,7 +172,36 @@ function App(): JSX.Element {
               <Button
                 title="Decrypt"
                 onPress={() => {
-                  RSA.decrypt(rsaEncryptedMessage, rsaPrivateKey).then(
+                  const privateKey2 = `-----BEGIN PRIVATE KEY-----
+                  MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDLuejh9ivsQh/c
+                  dTiMEDBLpLcppFTRI/c+6lPYujRBtMiNNXOXw3b5STCZM5yRb1ojbN6spnjRGNQU
+                  96HE8l6Lm/UYv1wREpjTrG1XndW8IAqw8kqvi3qqXVYQHmFlj6JP+UbpjZbXoYZv
+                  KB443HY0fkVT3VbWnUVztr063eQqULEQBwa/Kc7KQSACaTGSk99XnwbbBqE4bUv7
+                  IL1JyMTskRu1CHpENzmnGYxz1X5MRFD0Mvc8pBU40D7CnOpMqnZWp+C2SiJFBjZz
+                  jacuamd/jP1FiRNS+Zm8QeZk+NGVK3/FMh+fN4oKQfQIoxjvgxBXOCtH6cLx7m7h
+                  dohxE9FBAgMBAAECggEACS36UVwvRHkCSIO7pYwBMRdd7XFrJPVS6bHTT1usiR1l
+                  P/+dksQK81aZ6v1JW8BBtoDsU6k4nmtY+wzhbCdjnTTMyW2t71MAMeF+F8mT3OMw
+                  uCJivSAyycRk4VsNFm9vZ90aleF8X4D2tsF/Ax7j8J+T7iB2DjuN6pXcVu+ynTlq
+                  9SiFRRdDYMd2uWzRPkeq8Tqn08a536OD0X0ZyZ3ij0mYEpl+Td8D6RWm3ZuPVPzE
+                  OBoyeNSjzHmltAwssmhkst4qrsez4+vmGk6WCIVonVZGW4yPdcnYSiqoPxyWzKw2
+                  OloccoQLW1AjqOEjPxXuiLI+BC4Mmn+oUeTNwqPqMQKBgQDYtAHkXMbRobf5nUU/
+                  MkKBiPvVSYhqyNu7bcUI5icADTtamxdgEfYtJjNtHkX7Wk+GDTXvcu50Q4euh9S/
+                  76Ywyqq+xG2I9+56FyVZkkL4AJmkGUE+GnW9a9TPiHvdgplHcUcb114MY73BqG63
+                  kEiH2X3Yll9kg5UUD7NDwCZQdQKBgQDwq3mGHPy9LSTkFfGxyFr4TOUpNWDw9txA
+                  y7JJFvA1pKGqMq++yrd9BTpkDt7AtW/gacUQipagFMztBBcTr7Zp2RG3qmT+Ph0F
+                  mPHwlKkHDgBKtfQsv7o1mvxEkVdAFAs//pWN6Z3brgP7f5rAijZReWmjXeClTBVR
+                  Un0qNjNkHQKBgQDM1foj3f6hEPhSHnLiNaPfxZgmGAe2yH3LQQtKSiL7TYaZMph5
+                  2wCT1NQvhLopyUf6MmqVGFKF239ZXlOfyeuRm0CiahIPsivdhBtt+28J9adFEgo1
+                  pdU9Y7wZH63VyCkS+uR4UK85jt5DO+7L3LpyjwcBv8ITxL0Z5W1R0UNmMQKBgHvb
+                  w+8BfBDf6tx3bqvQMQEYkddjYUChyziNnHOBGp6A27J743aNXWE6D5LvFDSZK7qS
+                  tJiOunYoPk68jCPViktvK28agl9WGBmJXPozh8nZqszIV6GwCmUMHkgrhHu11RRa
+                  MR2X3WXxaJtGUJ5Ggy3rDjLFXZe0UERJ76CJxGkBAoGARmR6qBpeWkIyVxvOI9U5
+                  WGtuKrVtElx6Ui5hFlhOks74Qo4BG1k1giru7JuRwDrVHqnjtkFqQOvqEICcufy6
+                  vF1qRU406p73wg/Lk7mo8mSZQWIaG4S7JZdgVdlrY2Z5NI/MeGHqj6ZsCkWklGLx
+                  IcbCLW5PFiEm58YNW6/DNpU=
+                  -----END PRIVATE KEY-----`;
+                  const session = `PgCtHQW8W2OwRKzX+nSWk+vFd+Qtr8tS6SqGqnxOCKQ45h5TLAgfdOkFPbYVw9iOvqYJyVoetDVd8StC/U6T6B3nTNQug+fPf2Rx/orznjccyyR0a0IF+D6TnxizoUFuw4hR931BxzXwEqPDuhb8qx14+Bhub4vszjZ21TzHWCkIArtogW5qD94efYKc7HMYDkNdz4uQyyLBJbUeKG1yAwYvaw9o59QS0AK92qlL/b/0oKe9gSy9wjEk/gmCWF0F1oaeYA2ah8bj6DqSlcHB22aEps9zPYj2TDBegMxd1v/LYZA8Xch9RJ3144OpCFOBxYyU4OtSPP2W31gIiNxzfQ==`;
+                  RSA.decrypt(rsaEncryptedMessage, privateKey2).then(
                     decryptedMessage => {
                       setRSADecryptedMessage(decryptedMessage);
                       // console.log(`The original message was ${origianlMessage}`);
